07817813a1a8721aa50b6b1bbe570935363ec341
fukuikonmari <fukui.konmari@gmail.com>
2026-02-15 15:00:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
 const CACHE_NAME = 'totonoe-v1';
-const URLS_TO_CACHE = [
-  './totonoe-jikan.html',
-  './totonoe-jikan.png',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700;900&family=Noto+Sans+JP:wght@300;400;500;700&family=Michroma&family=EB+Garamond:ital,wght@0,400;0,600;1,400&display=swap'
-];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      const urls = ['./totonoe-jikan.html', './manifest.json'];
+      return Promise.allSettled(urls.map(url => cache.add(url)));
+    })
   );
   self.skipWaiting();
 });
@@ -24,6 +21,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
